4e3658ad14001462ae182f9ae104a6bec2588961
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2022-12-29 18:51:40 +0900

    [Silver IV] Title: 듣보잡, Time: 4016 ms, Memory: 53804 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,17 @@
+import re
+from unittest import result
+
+
+n,m = map(int,input().split())
+a= set()
+for i in range(n):
+    a.add(input())
+    
+b=set()
+for i in range(m):
+    b.add(input())
+result = sorted(list(a&b))
+print(len(result))
+
+for i in result:
+    print(i)
