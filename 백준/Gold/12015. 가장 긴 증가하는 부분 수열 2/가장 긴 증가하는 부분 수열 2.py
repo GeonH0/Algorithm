99f6679460c5a1ec8b0cb1fe0daef7059aa368f2
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-06-03 14:17:52 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 2564 ms, Memory: 155564 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,43 @@
+
+
+# n = int(input())
+# arr = list(map(int,input().split()))
+# d = [1 for i in range(n)]
+
+# for i in range(n):
+#     for j in range(i):
+#         if arr[j] < arr[i]:
+#             d[i] = j
+            
+        
+# print(max(d))
+
+
+
+
+n = int(input())
+arr = list(map(int,input().split()))
+LIS = [arr[0]]
+
+def binarySearch(e):
+    start = 0
+    end = len(LIS) - 1
+
+    while start <= end:
+        mid = (start + end ) //2
+
+        if LIS[mid] == e:
+            return mid
+        elif LIS[mid] < e:
+            start = mid +1
+        else:
+            end = mid -1
+    return start
+
+for i in range(n):
+    if arr[i] > LIS[-1]:
+        LIS.append(arr[i])
+    else:
+        idx = binarySearch(arr[i])
+        LIS[idx] = arr[i]
+print(len(LIS))
