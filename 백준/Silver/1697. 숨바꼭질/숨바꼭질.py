9858f99f8cf226c3d231f0840482643793ca7856
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-07-03 16:44:27 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 53128 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,32 +1,31 @@
-
-
 from collections import deque
 
 
 def bfs(s,e):
     q = deque()
-    v=[0]*2000001
-    
+    v=[0] * 2000001
     q.append(s)
-    v[s]=1
+    # 시작점 방문처리
+    
+    v[s] = 1    
     while q:
         c = q.popleft()
+        # 현재가 도착과 같으면 해당 위치를 찾은거기 때문에 해당 방문에서 -1
+        #-1하는 이유는 처음 시작 visited가 1이기 때문
         if c == e:
-            return v[c]-1
+            return v[c] - 1
         else:
-            for n in (c-1,c+1,c*2):
+            #아닐경우 3개의 가짓수를 돌면서
+            for n in(c-1,c+1,c*2):
+                #범위네에서 방문하지 않았다면
                 if 0<=n<=200000 and v[n] ==0:
+                    # Queue에 추가후
                     q.append(n)
+                    # 방문 처리
                     v[n] = v[c]+1
     return -1
-
-
-
-
-
 
 N,K = map(int,input().split())
 
 ans = bfs(N,K)
 print(ans)
-
