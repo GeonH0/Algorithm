0622827ce626e898eb609738987ee7a2c945ecfa
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-07-15 13:57:25 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,25 +1,24 @@
 
-
+# 백트래킹 ->  가능한 모든 경우
+# n 선택할 숫자, 종료 조건을 명확하게 하자 조건 n>N
+# lst 숫자 선택 변수
 
 def dfs(n,lst):
-
-    #종료 조건
+    # 종료 조건
     if n > N:
-        if len(lst) == M: #길이가 M이 되면
+        if len(lst) == M:
             ans.append(lst)
         return
-    dfs(n+1,lst+[n]) # 선택
-    dfs(n+1,lst)# 선택 하지 않을 경우
+    #하부 함수 호출 2가지
     
+    # 선택한 경우
+    dfs(n+1,lst+[n])
+    # 선택하지 않은 경우
+    dfs(n+1,lst)
 
 
-
-
-
+ans =[]
 N,M = map(int,input().split())
-
-
-ans=[]
 dfs(1,[])
 for i in ans:
     print(*i)
