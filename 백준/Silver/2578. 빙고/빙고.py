b573587aaac2c33e6fac10241276ee00501d9726
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-09-22 18:38:19 +0900

    [Silver IV] Title: 빙고, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,77 +1,46 @@
 
-
-
-cnt =0
-mp =[list(map(int,input().split())) for _ in range(5)]
-mc =[]
-
-
-
-for _ in range(5):
-    mc += list(map(int,input().split()))
-
-
-def check():
+def check_bingo():
     bingo = 0
-
-    for x in mp:
-        if x.count(0)==5:
+    for row in result:
+        if sum(row) == 5:
+            bingo += 1
+    for col in range(5):
+        if sum(result[row][col] for row in range(5)) == 5:
             bingo +=1
 
-    for i in range(5):
-        y = 0
-        for j in range(5):
-            if mp[j][i]==0:
-                y+=1
-        if y ==5:
-            bingo +=1
-
-    d1 = 0
-    for i in range(5):
-        if mp[i][i] == 0:
-            d1 += 1
-    
-    if d1 == 5:
+    if sum(result[i][i] for i in range(5)) == 5:
         bingo += 1
-
-    # 오른쪽위부터 시작하는 대각선 확인
-    d2 = 0
-    for i in range(5):
-        if mp[i][4-i] == 0:
-            d2 += 1
     
-    if d2 == 5:
+    if sum(result[i][4 - i] for i in range(5)) == 5:
         bingo += 1
+    
+    return bingo    
 
-    return bingo 
+
+my = [list(map(int,input().split())) for _ in range(5)]
+ans = [list(map(int,input().split())) for _ in range(5)]
+
+result = [[0 for _ in range(5)] for _ in range(5)]
+
+
+ans_dict = {}
+for x in range(5):
+    for y in range(5):
+        ans_dict[my[x][y]] = (x, y)
 
 cnt = 0
-for i in range(25):
-    for x in range(5):
-        for y in range(5):
-            if mc [i]== mp[x][y]:
-                mp[x][y]=0
-                cnt +=1
-    if cnt >=12:
-        result = check()
 
-        if result >=3:
-            print(i+1)
-            break
+for i in range(5):
+    for j in range(5):
+        cnt +=1
+        number = ans[i][j]
+        x,y = ans_dict[number]
 
-    
+        result[x][y] = 1
+        if check_bingo() >=3:
+            print(cnt)
+            exit()
 
-        
-
-    
-
-
-
-
-
-            
-    
-            
 
 
 
