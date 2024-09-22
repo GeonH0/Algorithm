02671b612b8b3df54c851083cdd7da9db812a4f1
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-09-22 16:30:56 +0900

    [Silver III] Title: 달팽이, Time: 1036 ms, Memory: 70036 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,51 +1,35 @@
-n  = int(input())
-m = int(input())
-arr =[[0]*n  for _ in range(n)]
+N = int(input())
+target = int(input())
 
-x= (n-1)//2
-y = (n-1)//2
+# 2차원 배열 생성
+snail = [[0] * N for _ in range(N)]
 
-#상, 하, 좌, 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-arr[x][y]=1
+# 시작 위치 (좌상단)
+x, y = 0, 0
+num = N * N
 
-i = 2
-start =3
+# 방향 (하, 우, 상, 좌)
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+direction = 0
 
-while x!=0 or y!=0:
-    while i<= start*start:
-        if x== y==(n-1)//2: #시작점
-            c_up,c_down,c_left,c_right = start,start-1,start-1,start-2
-            x+=dx[0]
-            y+=dy[0]
-            c_up -=1
-        elif c_right >0: #우
-            x+=dx[3]
-            y+=dy[3]
-            c_right-=1        
-        elif c_down>0:
-            x+=dx[1]
-            y+=dy[1]
-            c_down-=1
-        elif c_left>0:
-            x+=dx[2]
-            y += dy[2]
-            c_left -=1
+# 달팽이 채우기
+while num > 0:
+    snail[x][y] = num
+    if num == target:
+        target_x, target_y = x, y
+    
+    nx, ny = x + dx[direction], y + dy[direction]
+    if 0 <= nx < N and 0 <= ny < N and snail[nx][ny] == 0:
+        x, y = nx, ny
+    else:
+        direction = (direction + 1) % 4
+        x, y = x + dx[direction], y + dy[direction]
+    
+    num -= 1
 
+# 결과 출력
+for row in snail:
+    print(*row)
 
-        elif c_up>0:
-            x+=dx[0]
-            y+=dy[0]
-            c_up-=1
-        arr[x][y]=i
-        i+=1
-    n-=2
-    start+=2
-
-for j in range(len(arr)):
-    print(*arr[j])
-    if m in arr[j]:
-        mx = j+1
-        my = arr[j].index(m) +1
-print(mx,my)
+print(target_x + 1, target_y + 1)
