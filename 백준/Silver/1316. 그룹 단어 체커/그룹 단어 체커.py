0cf5470bc8a86837271233d1fe0ef5de97de689e
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-09-23 21:59:13 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,11 +1,28 @@
-n = int(input())
-cnt =n
-for i in range(n):
-    w = input()
-    for j in range(0,len(w)-1):
-        if w[j]==w[j+1]:
-            pass
-        elif w[j] in w[j+1:]:
-            cnt -=1
-            break
-print(cnt)
+from tabnanny import check
+
+
+N = int(input())
+ans = 0
+
+for _ in range(N):
+    word = input()
+    check = []
+    isgroup = True
+    if len(word) == 1:
+        ans +=1
+    else:
+        check.append(word[0])
+        for i in range(1,len(word)):
+            if word[i] == word[i-1]:
+                continue
+            else:
+                if word[i] not in check:
+                    check.append(word[i])
+                    continue
+                else:
+                    isgroup = False
+                    break
+        if isgroup:
+            ans +=1
+        
+print(ans)
