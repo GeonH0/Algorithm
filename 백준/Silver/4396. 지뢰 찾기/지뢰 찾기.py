ffe6932ab68de39de1875ec3471fee7c87ee082b
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-09-23 20:55:37 +0900

    [Silver IV] Title: 지뢰 찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.py
+++ 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.py
@@ -1,34 +1,37 @@
+def bombCheck(x, y):
+    cnt = 0
+    # 8방향 체크
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < N:  # 범위를 벗어나지 않는지 체크
+            if bomb[nx][ny] == '*':  # 주변에 지뢰가 있으면 카운트 증가
+                cnt += 1
+
+    return cnt  # 지뢰의 개수를 반환
+
+
 N = int(input())
 
-arr = [list(input()) for _ in range(N)]
-v = [list(input()) for _ in range(N)]
-ans = [['.']*N for _ in range(N)]
-dx = [-1, -1, -1, 0, 0, 1, 1, 1]
-dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+bomb = [list(input().strip()) for _ in range(N)]
+ans = [list(input().strip()) for _ in range(N)]
 
-for x in range(N):
-    for y in range(N):
-        if arr[x][y] == '.' and v [x][y] == 'x':
-            cnt = 0
+# 상 하 좌 우 우상대각 우하대각 좌상대각 좌하대각
+dx = [-1, 1, 0, 0, -1, 1, -1, 1]
+dy = [0, 0, -1, 1, 1, -1, -1, 1]
 
-            for i in range(8):
-                nx = x + dx[i]
-                ny = y + dy[i]
+result = [["."] * N for _ in range(N)]
 
-                if nx <0 or ny<0 or nx>=N or ny >=N:
-                    continue
-                if arr[nx][ny] == "*":
-                    cnt +=1
-            ans[x][y] = cnt
-        
-        if arr[x][y] == "*" and v[x][y] == "x":
-            for a in range(N):
-                for b in range(N):
-                    if arr[a][b] == "*":
-                        ans[a][b] = "*"
-
-                    
 for i in range(N):
     for j in range(N):
-        print(ans[i][j], end='')
-    print()                
+        if bomb[i][j] == '.' and ans[i][j] == 'x':  # 지뢰가 없으면서 열림
+            result[i][j] = str(bombCheck(i, j))            
+        if bomb[i][j] == '*' and ans[i][j] == 'x':  # 지뢰가 열렸으면 모든 지뢰를 표시
+            for a in range(N):
+                for b in range(N):
+                    if bomb[a][b] == '*':
+                        result[a][b] = '*'
+
+for row in result:
+    print("".join(map(str, row)))
+
