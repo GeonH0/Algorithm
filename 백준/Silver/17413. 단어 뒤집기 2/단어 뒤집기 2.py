299b93144b501dbd7723cdf2223b14a10d5d8f81
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-09-25 13:24:53 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 176 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,21 +1,28 @@
-W = list(input().rstrip())
 
-i=0 
-start = 0
+S = input()
 
-while i<len(W):
-    if W[i]=='<':
-        i+=1
-        while W[i] !='>':
-            i+=1
-        i+=1
-    elif W[i].isalnum():
-        start= i
-        while i< len(W) and W[i].isalnum():
-            i+=1
-        temp = W[start:i]
-        temp.reverse()
-        W[start:i]=temp
+revers = ""
+ans = ""
+is_Tag = False
+for i in S:    
+    if i =="<":
+        if revers:
+            ans += revers[::-1]
+            revers = ""
+        ans += i
+        is_Tag = True
+    elif i == ">":
+        ans +=i
+        is_Tag = False
+    elif is_Tag:
+        ans +=i
     else:
-        i+=1
-print("".join(W))
+        if i != " ":
+            revers += i
+        else:
+            ans += revers[::-1] + " "
+            revers = ""
+if revers:
+    ans += revers[::-1]    
+    
+print(ans)
