807c4aa3ec337dddf7e3a249c19b171ca70e03ef
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-09-25 14:22:34 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,32 +1,28 @@
-def change(a):
-    if arr[a] == 0:
-        arr[a] = 1
-    else:
-        arr[a] = 0
-    return
+def flip_bit(b):
+    return b ^ 1
 
 N = int(input())
-arr = list(map(int, input().split()))
-T = int(input())
 
-for _ in range(T):
-    x, y = map(int, input().split())
-    if x == 1:  # 남학생의 경우
-        for i in range(y - 1, N, y):
-            change(i)
-    else:  # 여학생의 경우
-        y -= 1
-        change(y)
-        for i in range(1, N // 2 + 1):
-            if y - i < 0 or y + i >= N:
-                break
-            if arr[y - i] == arr[y + i]:
-                change(y - i)
-                change(y + i)
-            else:
-                break
+status = list(map(int, input().split()))
+student = int(input())
 
-for i in range(N):
-    print(arr[i], end=' ')
-    if (i + 1) % 20 == 0:
-        print()
+for _ in range(student):
+    s, num = map(int, input().split())
+    if s == 1:
+        # 남학생의 경우
+        for i in range(len(status)):
+            if (i + 1) % num == 0:
+                status[i] = flip_bit(status[i])
+    elif s == 2:
+        # 여학생의 경우
+        for j in range(1, min(num, len(status) - num + 1)):
+            if num - 1 - j >= 0 and num - 1 + j < len(status):
+                if status[num - 1 - j] == status[num - 1 + j]:
+                    status[num - 1 - j] = flip_bit(status[num - 1 - j])
+                    status[num - 1 + j] = flip_bit(status[num - 1 + j])
+                else:
+                    break
+        status[num - 1] = flip_bit(status[num - 1])
+
+for i in range(0, len(status), 20):
+    print(" ".join(map(str, status[i:i+20])))
