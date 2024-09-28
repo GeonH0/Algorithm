83578c88523266a023944deaed637031c02d3e0d
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-09-28 15:16:58 +0900

    [Silver III] Title: 1로 만들기, Time: 504 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,16 +1,17 @@
-import sys
-input = sys.stdin.readline
+N = int(input())
 
-n = int(input())
-d=[0]*(n+1)
+dp = [0] * (N+1)
+if N >= 2:
+    dp[2] = 1
 
-for i in range(2,n+1):
-    d[i]=d[i-1]+1
-
-    if i%2==0 and d[i]>d[i//2]+1:
-        d[i]=d[i//2]+1
-    
-    if i%3==0 and d[i]>d[i//3]+1:
-        d[i]=d[i//3]+1
-
-print(d[n])
+# N이 3 이상일 때 초기화
+if N >= 3:
+    dp[3] = 1
+if N >= 4:
+    for i in range(4,N+1):        
+        dp[i] = dp[i-1] +1
+        if i %2 ==0:
+            dp[i] = min(dp[i],dp[i//2]+1)
+        if i %3 ==0:
+            dp[i] = min(dp[i],dp[i//3]+1)
+print(dp[N])
