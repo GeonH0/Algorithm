f2dcb78489e1bd73e97df692c17b70e538d71ee3
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-09-28 11:52:08 +0900

    [Bronze II] Title: 피보나치 수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,9 +1,12 @@
-n = int(input())
+N = int(input())
 
-dp = [1]*(n+1)
-dp[0]=0
-dp[1]=1
+dp = [0] * 46
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
 
-for i in range(2,n+1):
-    dp[i]= dp[i-1]+dp[i-2]
-print(dp[n])
+
+for i in range(3,N+1):
+    dp[i] = dp[i-1] + dp[i-2]    
+
+print(dp[N])
