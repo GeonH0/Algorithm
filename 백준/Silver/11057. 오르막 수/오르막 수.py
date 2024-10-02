9abcf99b8994f67a4a2afde0c022c9961895f760
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-10-02 23:07:43 +0900

    [Silver I] Title: 오르막 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,8 +1,14 @@
-n = int(input())
 
-dp = [1]*10
-for i in range(1,n):
-    for j in range(1,10):
-        dp[j]+=dp[j-1]
+N = int(input())
 
-print(sum(dp)%10007)
+dp = [[0] * 10 for _ in range(N + 1)]
+
+for i in range(10):
+    dp[1][i] = 1
+
+for i in range(2,N+1):
+    for j in range(10):
+        dp[i][j] = sum(dp[i-1][k] for k in range(j + 1))
+
+result = sum(dp[N]) % 10007
+print(result)
