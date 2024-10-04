d3698ea220875651f4ddaff8811094839d3e385d
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-10-04 23:05:00 +0900

    [Silver II] Title: 이동하기, Time: 1384 ms, Memory: 78676 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,15 +1,28 @@
 
-
-
 N,M = map(int,input().split())
 
-arr = [[0]*(M+1)] +[[0]+list(map(int,input().split())) for _ in range(N)]
+dp =[[0]*M for _ in range(N)]
 
-dp =[[0]*(M+1) for _ in range(N+1)]
+cnt = 0
+arr = [list(map(int,input().split())) for _ in range(N)]
 
-for i in range(1,N+1):
-    for j in range(1,M+1):        
-        dp[i][j] = arr[i][j]+ max(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+# DP 배열을 순차적으로 업데이트하면서 최대 사탕 개수 계산
+for i in range(N):
+    for j in range(M):
+        # 시작 위치 처리
+        if i == 0 and j == 0:
+            dp[i][j] = arr[i][j]
+        else:
+            # 위에서 오는 경우
+            if i >  0:
+                dp[i][j] = max(dp[i][j], dp[i-1][j] + arr[i][j])
+            # 왼쪽에서 오는 경우
+            if j > 0:
+                dp[i][j] = max(dp[i][j], dp[i][j-1] + arr[i][j])
+            # 대각선 위에서 오는 경우
+            if i > 0 and j > 0:
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + arr[i][j])
 
-
-print(dp[N][M])
+# (N, M) 위치에서의 최대 사탕 개수를 출력
+print(dp[N-1][M-1])
+        
