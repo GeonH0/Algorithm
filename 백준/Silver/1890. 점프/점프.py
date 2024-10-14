f82fec9570ad5d34a12a4396fe078850ac854cb2
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-10-14 23:48:08 +0900

    [Silver I] Title: 점프, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -1,24 +1,22 @@
-
-
 N = int(input())
+arr = []
+dp =[[0]*N for _ in range(N)]
+for _ in range(N):
+    arr.append(list(map(int,input().split())))
 
-arr = [list(map(int,input().split())) for _ in range(N)]
-
-jump =0
-dp = [[0]*(N) for _ in range(N)]
+#dp[i][j]는 (0,0)에서 시작하여 i,j로 오는 경로수
 dp[0][0] = 1
 
 for i in range(N):
     for j in range(N):
-        if dp[i][j] > 0 and arr[i][j] > 0:
+        if dp[i][j] > 0  and arr[i][j] > 0:
             jump = arr[i][j]
-            # 우측 경로 값 누적
+            # 우측으로 이동
             if j + jump < N:
                 dp[i][j+jump] += dp[i][j]
-            if i + jump < N:
+            # 아래로 이동
+            if i +jump < N:
                 dp[i+jump][j] += dp[i][j]
 
-
 print(dp[N-1][N-1])
-
-        
+            
