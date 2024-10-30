135321cfccd34ac7a7386f0907df04fd1d352541
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-10-30 18:57:22 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,346 ms, Memory: 183,932 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -4,17 +4,14 @@ T = int(input())
 for test_case in range(1,T+1):
     N = int(input())
     arr = list(map(int,input().split()))
-    s = 0
-
-    ans = 0
-    while s < N:
-        i_max = s
-        for i in range(s+1,N):
-            if arr[i_max] < arr[i]:
-                i_max = i
-        for i in range(s,i_max):
-            ans += (arr[i_max] - arr[i])
-        s = i_max+1
+    ans =0
+    arr = arr[::-1]
+    mx = 0
+    for n in arr:
+        if mx < n:
+            mx = n
+        else:
+            ans += (mx - n)
 
 
 
