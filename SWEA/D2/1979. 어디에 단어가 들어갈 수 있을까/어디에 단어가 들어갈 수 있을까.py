9efceb22e91fe7a0ea0b1af4b6e46cc8f86e353d
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2024-11-01 23:57:20 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 126 ms, Memory: 45,044 KB -BaekjoonHub

---
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -0,0 +1,24 @@
+
+def count(arr):
+    ret = 0
+    for lst in arr:
+        cnt = 0
+        for j in range(len(lst)):
+            if lst[j]:
+                cnt +=1
+            else:
+                if cnt == K:
+                    ret += 1
+                cnt =0 
+    return ret
+
+
+T = int(input())
+
+for test_case in range(1,T+1):
+    N,K = map(int,input().split())
+    arr = [list(map(int,input().split()))+[0] for _ in range(N)] +[[0]*(N+1)]
+    arr1 = list(map(list,zip(*arr)))
+    ans = count(arr) + count(arr1)
+
+    print(f'#{test_case} {ans}')
