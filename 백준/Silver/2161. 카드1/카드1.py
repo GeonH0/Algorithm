ae3ea7a27df38b9559370ebb4cbef74858d38e81
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-05 22:10:52 +0900

    [Silver V] Title: 카드1, Time: 60 ms, Memory: 35032 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,21 +1,21 @@
-n = int(input())
-arr=[]
-temp = [] 
-cnt =[]
-for i in range(n):
-    arr.append(i+1)
 
-while(len(arr)!=0):
-    if(len(arr)==1):
-        cnt.append(arr[0])
-        break
-    cnt.append(arr[0])
-    del arr[0]
-    
-    arr.append(arr[0])
-    
-    del arr[0]
-    
-for i in cnt:
-    print(i,end=" ")
+
+
+
+
+
+from collections import deque
+
+
+n = int(input())
+q = deque(range(1,n+1))
+
+discard = [] #버린카드 저장
+
+while len(q) > 1:
+    discard.append(q.popleft()) #제일 위 카드 버리고
+    q.append(q.popleft()) # 다음 카드 제이 ㄹ아래로 이동
+
+discard.append(q[0])
+print(*discard)
 
