085ae0ef985076663ce03f0d5a54dbacb1d526c1
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-07 20:32:35 +0900

    [Silver I] Title: 영역 구하기, Time: 68 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,46 +1,45 @@
+
+
 from collections import deque
 
 
-m,n,k = map(int,input().split())
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    graph[x][y] = 1
+    cnt = 1
+    while q:
+        cy,cx = q.popleft()
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0<= ny < M and 0<=nx<N and graph[ny][nx] == 0:               
+                q.append((ny,nx))
+                cnt +=1
+                graph[ny][nx] = 1
+    return cnt
+                
+        
 
-graph = [ [0] * n for _ in range(m)]
+M,N,K = map(int,input().split())
 
-for _ in range(k):
-    x1,y1,x2,y2 = map(int,input().split())
-    for i in range(x1,x2):
-        for j in range(m- y1 -1,m - y2 -1,-1):
-            graph[j][i]=1
-            
-
-dx=[-1,1,0,0]
+dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+graph = [[0]*N for _ in range(M)]
 
-    
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
-    graph[x][y]=1
-    size =1
-    while queue:
-        x,y = queue.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if 0<=nx < m and 0 <= ny <n and graph[nx][ny]==0:
-                graph[nx][ny]=1
-                queue.append((nx,ny))
-                size+=1
-    result.append(size)
+for _ in range(K):
+    lx,ly,rx,ry = map(int,input().split())
+    for y in range(ly,ry):
+        for x in range(lx,rx):
+            graph[y][x] = 1 
 
-result=[]
+ans = []            
+for i in range(M):
+    for j in range(N):
+        if graph[i][j] == 0:
+            ans.append(bfs(i,j))
 
-for i in range(m):
-    for j in range(n):
-        if graph[i][j]==0:
-            bfs(i,j)
-
-result.sort()
-print(len(result))
-for i in result:
-    print(i,end =' ')
+ans.sort()
+print(len(ans))
+print(" ".join(map(str,ans)))
