3ee062e77951516f4c0e52b1ba850b629d5946cd
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-10 14:33:11 +0900

    [Silver I] Title: 그림, Time: 300 ms, Memory: 35808 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,38 +1,40 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
+
+from collections import deque
 
 
-n,m = map(int,input().split())
-graph=[]
-for _ in range(n):
-    graph.append(list(map(int,input().split())))
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    arr[x][y] = 0 # 방문 처리
+    cnt = 1
+    while q:
+        cx,cy = q.popleft()
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0<= nx< N and 0 <= ny < M and arr[nx][ny] == 1:
+                q.append((nx,ny))
+                arr[nx][ny] = 0 # 해당 조건이 맞다면 방문 처리
+                cnt +=1
+    return cnt
 
-dx=[-1,1,0,0]
+
+
+dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def dfs(x,y):
-    global cnt
-    cnt +=1
-    graph[x][y]=0
+N,M = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(N)]
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny= y + dy[i]
-        if -1<nx<n and -1<ny<m and graph[nx][ny]==1:
-            dfs(nx,ny)
+ans = []
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 1:
+            ans.append(bfs(i,j))
 
-
-num =[]
-for i in range(n):
-    for j in range(m):
-        if graph[i][j]==1:
-            cnt =0
-            dfs(i,j)
-            num.append(cnt)
-if len(num)==0:
-    print(len(num))
+if len(ans) == 0:
+    print(0)
     print(0)
 else:
-    print(len(num))
-    print(max(num))
+    print(len(ans))
+    print(max(ans))
