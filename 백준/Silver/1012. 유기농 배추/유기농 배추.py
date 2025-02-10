d8e1b9f72d2b810a0bd26a1af1171e19d5d37220
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-10 23:29:18 +0900

    [Silver II] Title: 유기농 배추, Time: 220 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,19 +1,23 @@
+
 from collections import deque
 
 
 def bfs(x,y):
     q = deque()
     q.append((x,y))
-    graph[x][y] = 0 
+    graph[x][y] = 0
 
     while q:
         cx,cy = q.popleft()
         for i in range(4):
             nx = cx + dx[i]
             ny = cy + dy[i]
-            if 0<=nx<N and 0 <= ny <M and graph[nx][ny] == 1:
-                q.append((nx,ny))
-                graph[nx][ny] = 0
+
+            if 0<=nx<N and 0 <= ny <M:
+                if graph[nx][ny] == 1:
+                    q.append((nx,ny))
+                    graph[nx][ny] = 0
+    return
                 
 
     
@@ -25,18 +29,21 @@ T = int(input())
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+ans = []
 for _ in range(T):
+
     M,N,K = map(int,input().split())
     graph = [[0]*M for _ in range(N)]
     
-    for O in range(K):
+    for j in range(K):
         x,y = map(int,input().split())
-        graph[y][x] = 1 # 배추 표시는 1로 표시
+        graph[y][x] = 1
     cnt = 0
+
     for i in range(N):
         for j in range(M):
             if graph[i][j] == 1:
                 bfs(i,j)
-                cnt +=1
+                cnt += 1
     print(cnt)
 
