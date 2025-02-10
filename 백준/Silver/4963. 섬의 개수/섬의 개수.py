1da915a86b9b4b02bea477e06b32413ce5087ed0
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-10 23:29:39 +0900

    [Silver II] Title: 섬의 개수, Time: 84 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,28 +1,46 @@
-dx = [1, -1, 0, 0, 1, -1, 1, -1]
-dy = [0, 0, -1, 1, -1, -1, 1, 1]
-def bfs(i, j):
-    s[i][j] = 0
-    queue = [[i, j]]
-    while queue:
-        a, b = queue[0][0], queue[0][1]
-        del queue[0]
-        for k in range(8):
-            x = a + dx[k]
-            y = b + dy[k]
-            if 0 <= x < h and 0 <= y < w and s[x][y] == 1:
-                s[x][y] = 0
-                queue.append([x, y])
+
+
+from collections import deque
+
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    arr[x][y] = 0
+
+    while q:
+        cx,cy = q.popleft()
+        for i in range(8):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0 <= nx < w and 0 <= ny < h and arr[nx][ny] == 1:
+                q.append((nx,ny))
+                arr[nx][ny] = 0             
+
+
+
+
+
+dx = [-1,1,0,0,1,-1,1,-1]
+dy = [0,0,-1,1,1,1,-1,-1]
+ans = []
+
 while True:
-    w, h = map(int, input().split())
+    h,w = map(int,input().split())    
     if w == 0 and h == 0:
         break
-    s = []
-    cnt = 0
-    for i in range(h):
-        s.append(list(map(int, input().split())))
-    for i in range(h):
-        for j in range(w):
-            if s[i][j] == 1:
-                bfs(i, j)
-                cnt += 1
-    print(cnt)
+    else:        
+        arr = [list(map(int,input().split())) for _ in range(w)]
+        cnt = 0
+        for i in range(w):
+            for j in range(h):
+                if arr[i][j] == 1:
+                    bfs(i,j)
+                    cnt += 1
+        ans.append(cnt)
+
+for i in ans:
+    print(i)
+        
+
+
