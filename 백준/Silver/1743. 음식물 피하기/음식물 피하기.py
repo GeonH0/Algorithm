a588b9804b7c43c1e9526bd0160da23fae2ac59e
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-12 19:29:37 +0900

    [Silver I] Title: 음식물 피하기, Time: 192 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,37 +1,40 @@
-import sys
-sys.setrecursionlimit(10**7)
-input = sys.stdin.readline
 
 
+from collections import deque
+
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    graph[x][y] = 0 # 방문 처리를 위해 0으로 변경
+    cnt = 1
+    while q:
+        cx,cy = q.popleft()
+        for i in range(4):
+            nx = dx[i] + cx
+            ny = dy[i] + cy
+            if 0<=nx< n and 0 <= ny < m and graph[nx][ny] == 1:
+                cnt +=1
+                q.append((nx,ny))
+                graph[nx][ny] = 0
+
+    return cnt
+
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 n,m,k = map(int,input().split())
-size =0
-ans =0
 
-
-graph = [[0]*(m+1) for _ in range(n+1)]
-
+graph = [[0]* m for _ in range(n)]
 
 for _ in range(k):
     r,c = map(int,input().split())
-    graph[r-1][c-1]=1
+    graph[r-1][c-1] = 1
 
-def dfs(x,y):
-    global size
-    if x<0 or x>n or y<0 or y>m:
-        return False
-    if graph[x][y]==1:
-        size +=1
-        graph[x][y]=0
-        dfs(x+1,y)
-        dfs(x-1,y)
-        dfs(x,y+1)
-        dfs(x,y-1)
-        return True
-    return False
+ans = 0
 
 for i in range(n):
     for j in range(m):
-        if dfs(i,j) == True:
-            ans = max(size,ans)
-            size=0
+        if graph[i][j] == 1:
+            ans = max(bfs(i,j),ans)
 print(ans)
