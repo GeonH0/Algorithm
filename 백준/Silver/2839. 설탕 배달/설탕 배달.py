ad3aa9a0e4e0515176776b6fe93aff0c8f94609b
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-13 19:15:07 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -7,8 +7,11 @@ if N >= 5:
 
 for i in range(6, N + 1):  # 6부터 N까지 계산
     if dp[i - 3] != -1:  # 3kg 봉지를 사용할 수 있다면
-        dp[i] = dp[i - 3] + 1
+        dp[i] = dp[i - 3] + 1    
     if dp[i - 5] != -1:  # 5kg 봉지를 사용할 수 있다면
-        dp[i] = min(dp[i], dp[i - 5] + 1) if dp[i] != -1 else dp[i - 5] + 1
+        if dp[i] == -1:  # 만약 아직 dp[i]가 초기화되지 않았다면
+            dp[i] = dp[i - 5] + 1
+        else:  # 이미 dp[i]가 초기화된 상태라면 최소값을 선택
+            dp[i] = min(dp[i], dp[i - 5] + 1)
 
 print(dp[N])
