7382d6dcf27fc1a71646d81d92b9b7ab4281ba02
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-13 21:01:47 +0900

    [Silver IV] Title: 암기왕, Time: 5788 ms, Memory: 199912 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,15 +1,27 @@
-import sys
-input = sys.stdin.readline
-t= int(input())
 
+def binary_search(arr,target):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) //2
+        if arr[mid] == target:
+            return True
+        if arr[mid] > target:
+            end = mid -1
+        else:
+            start = mid +1
 
-for _ in range(t):
-    n = int(input())
-    s1 = set(map(int,input().split()))
-    m= int(input())
-    s2 = list(map(int,input().split()))
-    for i in s2:
-        if i in s1:
+    return False
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    note1 = list(map(int,input().split()))
+    note1.sort()
+    M = int(input())
+    note2 = list(map(int,input().split()))
+    for i in note2:
+        if binary_search(note1,i):
             print(1)
         else:
             print(0)
