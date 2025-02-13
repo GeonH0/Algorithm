87aa879ead0c9841a50678c6d654d2bc088fd01e
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-13 19:49:59 +0900

    [Silver V] Title: 숫자 카드, Time: 1812 ms, Memory: 109836 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,25 +1,31 @@
-import sys
-input = sys.stdin.readline
 
-n = int(input())
-arr= list(map(int,input().split()))
-m = int(input())
-check = list(map(int,input().split()))
-arr.sort()
+def binary_search(arr,target):
+    start = 0
+    end = len(arr)- 1
+    
 
-def b_search(array,target,start,end):
-    while start<=end:
-        mid = (start+end)//2
-
-        if array[mid]==target:
-            return mid
-        elif array[mid]>target:
-            end = mid-1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return True
+        if arr[mid] > target:
+            end = mid - 1
         else:
-            start=mid+1
-    return None
-for i in range(m):
-    if b_search(arr,check[i],0,n-1) is not None:
-        print(1,end =" ")
+            start = mid + 1
+    return False
+        
+        
+N = int(input())
+card = list(map(int,input().split()))
+card.sort()
+
+M = int(input())
+serach = list(map(int,input().split()))
+ans = []
+for i in serach:
+    if binary_search(card,i):        
+        ans.append(1)
     else:
-        print(0,end= " ")
+        ans.append(0)
+
+print(" ".join(map(str,ans)))
