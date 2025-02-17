fb2930917517cd690599263066d695261fb3d897
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-17 19:04:03 +0900

    [Gold IV] Title: 최단경로, Time: 592 ms, Memory: 69516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,36 +1,41 @@
 
 
-
 import heapq
 import sys
-INF = sys.maxsize
 
-def dijkstra(s):
-    heap  =[]
-    v[s]=0
-    heapq.heappush(heap,(0,s))
-
-    while heap:
-        c,n = heapq.heappop(heap)
-
-        if v[n]<c:
-            continue
-        for nn,w in arr[n]:
-            nc = c+w
-            if nc< v[nn]:
-                v[nn] = nc
-                heapq.heappush(heap,(nc,nn))
-
+input = sys.stdin.readline
 
 V,E = map(int,input().split())
-k = int(input())
-arr=[[] for _ in range(V+1)]
-v=[INF]*(V+1)
+start = int(input())
+
+INF = 1e9
+
+graph = [[] for _ in range(V+1)]
+distance = [INF] * (V+1)
 
 for _ in range(E):
-    x,y,w = map(int,input().split())
-    arr[x].append((y,w))
-    
-dijkstra(k)
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
+
+
+def dijkstar(start):
+    q = []
+    heapq.heappush(q,(0,start))
+    distance[start] = 0
+    while q:
+        dist,now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q,(cost,i[0]))
+
+dijkstar(start)
+
 for i in range(1,V+1):
-    print("INF" if v[i]== INF else v[i])
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
