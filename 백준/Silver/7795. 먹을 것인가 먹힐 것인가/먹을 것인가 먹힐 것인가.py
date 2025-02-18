2827c0923b978c294f37784095b5c99a50a26371
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-18 12:26:25 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 124 ms, Memory: 37736 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,27 +1,23 @@
-t= int(input())
+
+import bisect
+import sys
 
 
+input = sys.stdin.readline
 
-def binary_search(tar,data):
-    start = 0
-    end = len(data)-1
-    res = -1
-    while start<=end:
-        mid = (start+end)//2
-        if data[mid]<tar:
-            res = mid
-            start = mid+1
-        else:
-            end = mid-1
-    return res
-for i in range(t):
-    n,m = map(int,input().split())
-    a=list((map(int,input().split())))
-    b=list((map(int,input().split())))
-    cnt =0
-    a.sort()
-    b.sort()
+T = int(input())
 
-    for i in a:
-        cnt += binary_search(i,b)+1
+for _ in range(T):
+    N,M = map(int,input().split())
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
+    A.sort()
+    B.sort()
+
+    cnt = 0
+    for a in A:
+        cnt += bisect.bisect_left(B,a)
     print(cnt)
+        
+
+            
