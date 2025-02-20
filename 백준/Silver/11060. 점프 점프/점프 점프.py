85570abfb944fa56bf2b6d86ec0436b89981c1ae
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-02-20 19:31:27 +0900

    [Silver II] Title: 점프 점프, Time: 68 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -1,18 +1,31 @@
-n = int(input())
-arr= list(map(int,input().split()))
-dp =[n+1]*n
-dp[0]=0
 
-for i in range(n):
-    for j in range(1,arr[i]+1):
-        if i+j<n:
-            dp[i+j]=min(dp[i]+1,dp[i+j])
-if dp[n - 1] == n + 1:
-	print(-1)
-else:
-	print(dp[n - 1])
-    
+from collections import deque
+import sys
 
 
+def bfs():
+    q = deque()
+    q.append((0,0)) # 현재 위치, 점프 횟수
+
+    visited[0] = True
+
+    while q:
+        cx,cnt = q.popleft()
+
+        if cx == N-1:
+            return cnt
+        for i in range(cx+1,min(cx+arr[cx]+1,N)): # 현재칸에서 갈수 있는 모든 범위 탐색
+            if not visited[i]:
+                visited[i] = True
+                q.append((i,cnt+1))
+    return -1
 
 
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int,input().split()))
+visited = [False] * N
+
+print(bfs())
