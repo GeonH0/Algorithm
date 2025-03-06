8f1d7e4436856aaab5d0e68c7e18d250b2368be1
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-03-06 21:44:51 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,52 +1,66 @@
 
+
 from collections import deque
+import sys
 
 
-def bfs(si,sj):
-
+def bfs(x,y,color,mod):
     q = deque()
-    q.append((si,sj))
-    v[si][sj]=1
-    while q:
-        ci,cj = q.popleft()
-        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            ni, nj = ci + di, cj + dj
-            # 범위내, 미방문, 현재 좌표와 다음 좌표가 같을경우
-            if 0 <= ni < N and 0 <= nj < N and v[ni][nj]==0 and arr[ni][nj]== arr[ci][cj] :
-                v[ni][nj]=1
-                q.append((ni,nj))
-
+    q.append((x,y))
+    if mod == 0:
+        visited[x][y] = 1
+    else:
+        visitedR[x][y] = 1
     
+    while q:
+        cx,cy = q.popleft()
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0<=nx<N and 0<=ny<N:
+                if mod == 0: # 적록색약이 아닐경우
+                    if arr[nx][ny] == color and visited[nx][ny] == 0:                
+                        visited[nx][ny] = 1
+                        q.append((nx,ny))
+                else:
+                    if (color == 'R' or color == 'G'): # 현재 색상이 빨강이거나 초록일떄
+                        if (arr[nx][ny] == 'R' or arr[nx][ny] == 'G') and visitedR[nx][ny] == 0:
+                            visitedR[nx][ny] = 1
+                            q.append((nx,ny))
+                    elif arr[nx][ny] == color and visitedR[nx][ny] == 0:
+                        visitedR[nx][ny] = 1
+                        q.append((nx,ny))
 
+                                    
+                    
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
+input = sys.stdin.readline
 
-N= int(input())
-arr=[list(map(str,input())) for _ in range(N)]
-v=[[0]*N for _ in range(N)]
+N = int(input())
 
-# 적록색약 아닌 경우
-cnt1 =0
+arr = [list(map(str,input().strip())) for _ in range(N)]
+visited =[[0]*N for _ in range(N)]
+visitedR = [[0]*N for _ in range(N)]
+ans1,ans2 = 0,0
 
+# 일반인 BFS 탐색
 for i in range(N):
     for j in range(N):
-        if v[i][j] == 0:
-            bfs(i,j)
-            cnt1 +=1
+        if visited[i][j] == 0:
+            bfs(i, j, arr[i][j], 0)
+            ans1 += 1
 
-# 적록색약인 경우 빨강을 찾을경우 초록으로 변경시킨다.
-
+# 적록색약 BFS 탐색
 for i in range(N):
     for j in range(N):
-        if arr[i][j] =='R':
-            arr[i][j] ='G'
-
-v=[[0]*N for _ in range(N)]
-cnt2 =0
-for i in range(N):
-    for j in range(N):
-        if v[i][j] == 0:
-            bfs(i,j)
-            cnt2 +=1
+        if visitedR[i][j] == 0:
+            bfs(i, j, arr[i][j], 1)
+            ans2 += 1
 
 
-print(cnt1,cnt2)
+
+print(ans1,ans2)
+            
+            
