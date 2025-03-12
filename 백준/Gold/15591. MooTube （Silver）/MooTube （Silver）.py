f92da052288f8d52aac87bf89c6e030204ffaa19
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-03-12 22:44:06 +0900

    [Gold V] Title: MooTube (Silver), Time: 3260 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -1,38 +1,32 @@
-
-
-
 from collections import deque
+import sys
 
+input = sys.stdin.readline
 
-def bfs(k,v):
-    vv=[0]*(N+1)
+def bfs(start,k):
     q = deque()
-    cnt =0
-
-    q.append(v)
-    vv[v]=1
+    q.append(start)
+    visited = [False] *(N+1)
+    visited[start] = True
+    cnt = 0
     while q:
-        c = q.popleft()
-        for i in arr[c]:
-            if vv[i[0]] ==0 and i[1]>=k:
-                q.append(i[0])
+        node = q.popleft()
+        for n,u in arr[node]:
+            if not visited[n] and u >= k:
+                visited[n] = True
+                q.append(n)
                 cnt +=1
-                vv[i[0]]=1
     return cnt
-                
-
-
-
 
 N,Q = map(int,input().split())
-arr=[[] for _ in range(N+1)]
-
+arr= [[]for _ in range(N+1)]
 for _ in range(N-1):
-    x,y,r = map(int,input().split())
-    arr[x].append((y,r))
-    arr[y].append((x,r))
-    
+    p,q,r = map(int,input().split())
+    arr[p].append((q,r))
+    arr[q].append((p,r))    
+
 
 for _ in range(Q):
     k,v = map(int,input().split())
-    print(bfs(k,v))
+    print(bfs(v,k))
+    
