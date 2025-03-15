932bcc902406238f3afaea5f4ab0605c4ef0eac8
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-03-15 23:00:45 +0900

    [Gold IV] Title: 빙산, Time: 4672 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,71 +1,76 @@
 
-
-
-
-
 from collections import deque
 
 
-def bfs(si,sj,v):
-    q = deque()
-
-    q.append((si,sj))
-    v[si][sj]=1
+def melt():
+    bing_cnt = [[0]*M for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] > 0: # 빙산이 있으면
+                 for k in range(4): # 4방향 탐색
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    if 0<=nx<N and 0<=ny<M and arr[nx][ny] == 0:
+                        bing_cnt[i][j] +=1
     
+    # 빙하를 한꺼번에 다 녹여줌
+    for i in range(N):
+        for j in range(M):
+            arr[i][j] -= bing_cnt[i][j]
+            if arr[i][j] <0:
+                arr[i][j] =0
+
+            
+def bfs(x,y,visited):
+    q = deque()
+    q.append((x,y))
+    visited[x][y] = True
 
     while q:
-        ci,cj = q.popleft()
-        # 4방향, 미방문, arr 값이 0보다 클경우
-        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)): #상, 하, 좌, 우
-            ni,nj = ci+di,cj+dj
-            if  arr[ni][nj] > 0 and v[ni][nj]==0:
-                q.append((ni,nj))
-                v[ni][nj] = 1
+        cx,cy = q.popleft()
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0<=nx<N and 0<=ny<M and not visited[nx][ny] and arr[nx][ny] > 0:
+                visited[nx][ny] = True
+                q.append((nx,ny))
+
                 
+def count_bing():
+    visited =[[False]*M for _ in range(N)]
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] >0 and not visited[i][j]:
+                bfs(i,j,visited)
+                cnt +=1
+                if cnt >=2:
+                    return cnt
+    return cnt
 
+    
 
+            
 
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]                
+    
 
 
 N,M = map(int,input().split())
-arr=[list(map(int,input().split())) for _ in range(N)]
-v=[[0]*M for _ in range(N)]
+arr = [list(map(int,input().split())) for _ in range(N)]
+year = 0  # 시간 (빙산이 녹는 년도)
 
+while True:
+    melt()  # 빙산이 한 번 녹음
+    year += 1  # 1년 경과
 
-def solve():# 년, 전체순회 반복
-    for y in range(1,90000):
-        #4방향 0의 갯수 count
-        a_sub = [[0]*M for _ in range(N)]
-        # 가장자리는 제외하자
-        for i in range(1,N-1):
-            for j in range(1,M-1):
-                if arr[i][j]==0:
-                    continue
-                for di,dj in((-1,0),(1,0),(0,-1),(0,1)):
-                    ni,nj = i+di,j+dj
-                    if arr[ni][nj]==0:
-                        a_sub[i][j]+=1
-        # 높이 낮추기
-        for i in range(1,N-1):
-            for j in range(1,M-1):
-                if a_sub[i][j]>0:
-                    arr[i][j]=max(0,arr[i][j]-a_sub[i][j])
-
-        #빙산 덩어리 개수 count
-        v=[[0]*M for _ in range(N)]
-        cnt =0
-        for i in range(1,N-1):
-            for j in range(1,M-1):
-                if v[i][j]==0 and arr[i][j]>0:
-                    bfs(i,j,v)
-                    cnt +=1
-                    if cnt>1: # 덩어리 갯수 2 이상
-                        return y
-        #덩어리 갯수가 0이면 0으로 return 
-        if cnt ==0:
-            return 0
-
-        
-
-ans = solve()
-print(ans)
+    # 빙산 덩어리 개수 확인
+    iceberg_count = count_bing()
+    
+    if iceberg_count == 0:
+        print(0)
+        break
+    elif iceberg_count >= 2:
+        print(year)
+        break
