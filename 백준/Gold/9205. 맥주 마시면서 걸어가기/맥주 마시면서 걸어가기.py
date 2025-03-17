252739f9bb5574807553b1851352897dac8aa6d7
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-03-17 20:19:19 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 120 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -3,46 +3,35 @@
 from collections import deque
 
 
-def bfs(x,y,ex,ey):
+def bfs():
     q = deque()
-    v=[0]*N
+    q.append(0)
+    visited[0] = True
 
-    q.append((x,y))
-    
     while q:
-
-        cx,cy = q.popleft()
-        #미방문 편의점 좌표를 범위내에 있는지 체크한다.
-        if abs(cx-ex)+abs(cy-ey)<=1000: #목적지에 도달이 가능하다면 정답 처리
-                        
+        idx = q.popleft()
+        x,y = location[idx]
+        
+        if idx == n+1:
             return 'happy'
 
-        for i in range(N):
-            if v[i]==0:
-                nx,ny  = arr[i]
-                if abs(cx-nx)+abs(cy-ny)<=1000: #범위 내에 있는것
-                    q.append((nx,ny))
-                    v[i]=1
-                    
+        for i in range(n+2):
+            if not visited[i]:
+                nx,ny = location[i]
+                if abs(nx - x) + abs(ny - y) <= 1000:  # 1000m 이내 이동 가능
+                    visited[i] = True
+                    q.append(i)
     return 'sad'
-         
-
-
-
+    
 
 
 
 t = int(input())
-
 for _ in range(t):
-    N = int(input())
-    si,sj = map(int,input().split())
-    arr=[]
-    
-    for _ in range(N):
-        arr.append(list(map(int,input().split())))
-    ei,ej = map(int,input().split())
-    ans = bfs(si,sj,ei,ej)
-    print(ans)
-
-
+    n = int(input())
+    location = []
+    visited = [False] *(n+2)    
+    for k in range(n+2):
+        x,y = map(int,input().split())
+        location.append((x,y))    
+    print(bfs())
