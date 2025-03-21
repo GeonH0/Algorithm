642ab1e3769a3a2fcda29c0ea569e84b88d890e9
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-03-21 20:15:15 +0900

    [Bronze I] Title: 단어 공부, Time: 212 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,18 +1,16 @@
+N = str(input())
+cnt = {}
+for i in N.upper():
+      
+    if i not in cnt:
+        cnt[i] = 1
+    else:
+        cnt[i] +=1
 
+ans = max(cnt.values())
+key = [k for k, v in cnt.items() if v == ans]
 
-
-
-
-
-R = input().upper()
-Rlist = list(set(R))
-cnt =[]
-
-for i in Rlist:
-    count = R.count(i)
-    cnt.append(count)
-
-if cnt.count(max(cnt))>=2:
-    print("?")
+if len(key) > 1:
+    print('?')
 else:
-    print(Rlist[(cnt.index(max(cnt)))])
+    print(key[0])
