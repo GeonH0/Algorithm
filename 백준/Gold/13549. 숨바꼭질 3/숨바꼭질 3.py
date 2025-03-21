a6158508673f636981c5fe9de402c7517f618416
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-03-21 18:01:40 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 35640 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,27 +1,31 @@
+
 from collections import deque
 
-def bfs(s,e):
-    q = deque()
-    
-    q.append(s)
-    v[s]=0
-    while q:
-        c = q.popleft()
-        if c == e:
-            return v[c]
-        else:
-            for n in (c-1,c+1,c*2):
-                if 0 <= n <= 100000 and v[n] == -1:
-                    # 순간이동이라면
-                    if n == c * 2:
-                        v[n] = v[c] # 0초 추가 
-                        q.appendleft(n) # 순간이동이기에 먼저 탐색
 
-                    else: 
-                        v[n] = v[c] + 1
-                        q.append(n)
+def bfs(start,end):
+    visited = [False] *100001
+    q = deque()
+    q.append((start,0))
+    visited[start] = True
+
+    while q:
+        cx,cnt = q.popleft()
+        if cx == end:            
+            return cnt
+        else:
+            for i in (cx * 2, cx - 1, cx + 1):
+                    if 0<=i <=100000 and not visited[i]:
+                        visited[i] = True
+                        if i == cx*2:                            
+                            q.appendleft((i,cnt))
+                        else:                            
+                            q.append((i,cnt+1))
+
+
+
+
+    
+
 
 N,K = map(int,input().split())
-v=[-1]*20000001
-ans = bfs(N,K)
-print(ans)
+print(bfs(N,K))
