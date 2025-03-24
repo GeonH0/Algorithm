95c2b138f8e49c72bb4c46bf8c6bdc0593ad98a5
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-03-24 19:43:31 +0900

    [Silver V] Title: 올림픽, Time: 32 ms, Memory: 32412 KB, Score: 8 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,17 +1,24 @@
-import sys
-input = sys.stdin.readline
+N,K = map(int,input().split())
 
-n,k = map(int,input().split())
-arr=[]
-for _ in range(n):
-    arr.append(list(map(int,input().split())))
+arr = []
 
-arr.sort(key = lambda x : (x[1],x[2],x[3]),reverse=True)
+for _ in range(N):
+    num,gold,sliver,bronze = map(int,input().split())
+    arr.append((num,gold,sliver,bronze))
 
-for i in range(n):
-    if arr[i][0] == k:
-        index = i
-for j in range(n):
-    if arr[index][1:] == arr[j][1:]:
-        print(j+1)
+arr.sort(key = lambda x: (-x[1],-x[2],-x[3]))
+
+rank = 1
+same = 1
+
+for i in range(N):
+    if arr[i][0] == K:
+        print(rank)
         break
+    
+    # 메달 순위가 같으면 rank 유지
+    if i > 0 and arr[i][1:] == arr[i-1 ][1:]:
+        same+= 1
+    else:
+        rank = i+1
+        same = 1
