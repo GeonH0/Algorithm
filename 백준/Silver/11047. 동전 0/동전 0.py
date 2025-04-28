51796fc4fb00bfc53d95338a871f3154ca923c74
GeonH0 <88571960+GeonH0@users.noreply.github.com>
2025-04-28 17:17:27 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,13 +1,17 @@
+
 import sys
+
 
 input = sys.stdin.readline
 
-n,k=map(int,input().split())
-a=list()
-for i in range(n):
-    a.append(int(input()))
-c=0
-for i in reversed(range(n)):
-    c+=k//a[i]
-    k= k%a[i]
-print(c)
+N,K = map(int,input().split())
+arr = [int(input()) for _ in range(N)]
+arr.sort(reverse=True)
+j = 0
+cnt = 0
+for i in range(N):
+    if K/arr[i] >=1:
+        cnt += K//arr[i]
+        K = K%arr[i]        
+print(cnt)
+
